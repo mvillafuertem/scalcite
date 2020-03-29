712ac40b39bbd13e7ca4ec0becdf091140da80ab
Scala Steward <me@scala-steward.org>
2020-03-29 16:26:32 +0200

    Update jsoniter-scala-core, ... to 2.1.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -159,7 +159,7 @@ object Dependencies {
     val circe = "0.13.0"
     val h2 = "1.4.200"
     val json4s = "3.6.7"
-    val jsoniter = "2.1.7"
+    val jsoniter = "2.1.10"
     val logback = "1.2.3"
     val mapflablup = "0.1.1"
     val scalaLogging = "3.9.2"
