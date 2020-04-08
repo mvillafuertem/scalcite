5c86adcab4c2888efc4d8c6a9c8a6cda1e177867
Miguel Villafuerte <mivimur@hotmail.com>
2020-04-08 23:24:10 +0200

    refactor(scalcite-example) : integrate with zio layer

--- scalcite-example/backend/src/main/scala/io/github/mvillafuertem/scalcite/example/ScalciteServiceApplication.scala
+++ scalcite-example/backend/src/main/scala/io/github/mvillafuertem/scalcite/example/ScalciteServiceApplication.scala
@@ -1,6 +1,6 @@
 package io.github.mvillafuertem.scalcite.example
 import akka.actor.typed.ActorSystem
-import io.github.mvillafuertem.scalcite.example.configuration.{AkkaConfiguration, ScalciteServiceConfiguration}
+import io.github.mvillafuertem.scalcite.example.configuration.ScalciteServiceConfiguration
 import zio._
 import zio.clock.Clock
 import zio.console.Console
@@ -17,20 +17,21 @@ object ScalciteServiceApplication extends ScalciteServiceConfiguration with zio.
   private val loggingLayer: URLayer[Console with Clock, Logging] =
     Logging.console((_, logEntry) => logEntry)
 
-  private val actorSystemLayer: RLayer[Has[ExecutionContext], Has[ActorSystem[_]]] =
-    ZLayer.fromAcquireRelease(actorSystem)(sys => UIO.succeed(sys.terminate()).ignore)
-
   private val executionContextLayer: ULayer[Has[ExecutionContext]] =
     ZLayer.succeed(platform.executor.asEC)
 
-  val program: ZIO[Has[ActorSystem[_]], Throwable, Int] =
+  private val live: TaskLayer[Has[ActorSystem[_]]] =
+    (executionContextLayer >>> actorSystemLayer)
+
+  val program: ZIO[Logging, Nothing, Int] =
     (for {
       _ <- httpServer
     } yield 0)
+      .provideLayer(live)
+      .foldM(e => log.throwable("", e).as(1), _ => UIO.effectTotal(0))
 
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
-    program
-      .provideLayer(executionContextLayer >>> actorSystemLayer)
-      .foldM(e => log.throwable("", e).as(1), _ => UIO.effectTotal(0))
-      .provideLayer(loggingLayer)
+    program.provideLayer(loggingLayer)
+
+
 }
--- scalcite-example/backend/src/main/scala/io/github/mvillafuertem/scalcite/example/configuration/AkkaConfiguration.scala
+++ scalcite-example/backend/src/main/scala/io/github/mvillafuertem/scalcite/example/configuration/AkkaConfiguration.scala
@@ -1,16 +1,74 @@
 package io.github.mvillafuertem.scalcite.example.configuration
 
-import io.github.mvillafuertem.scalcite.example.configuration.properties.ScalciteConfigurationProperties
+import akka.actor.BootstrapSetup
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.adapter._
+import akka.http.scaladsl.Http
+import akka.stream.Materializer
+import akka.{Done, actor}
+import io.github.mvillafuertem.scalcite.example.api.SwaggerApi
+import zio._
 
 import scala.concurrent.ExecutionContext
 
 
-final class AkkaConfiguration(executionContext: ExecutionContext,
-                              scalciteConfigurationProperties: ScalciteConfigurationProperties) {
+trait AkkaConfiguration {
+  self: ApiConfiguration with InfrastructureConfiguration =>
 
-}
+  val actorSystemLayer: RLayer[Has[ExecutionContext], Has[ActorSystem[_]]] =
+    ZLayer.fromAcquireRelease(actorSystem)(sys => UIO.succeed(sys.terminate()).ignore)
 
-object AkkaConfiguration {
+  private lazy val actorSystem: RIO[Has[ExecutionContext], ActorSystem[_]] =
+    for {
+      executionContext <- ZIO.access[Has[ExecutionContext]](_.get)
+      actorSystem <- Task(
+        ActorSystem[Done](
+          Behaviors.setup[Done] { context =>
+            context.setLoggerName(this.getClass)
+            context.log.info(s"Starting ${scalciteConfigurationProperties.name}... ${"BuildInfo.toJson"}")
+            Behaviors.receiveMessage {
+              case Done =>
+                context.log.error(s"Server could not start!")
+                Behaviors.stopped
+            }
+          },
+          scalciteConfigurationProperties.name.toLowerCase(),
+          BootstrapSetup().withDefaultExecutionContext(executionContext)
+        )
+      )
+    } yield actorSystem
 
+
+
+
+  val httpServer: RIO[Has[ActorSystem[_]], Unit] =
+    for {
+      actorSystem <- ZIO.access[Has[ActorSystem[_]]](_.get)
+      eventualBinding <- Task {
+        implicit lazy val untypedSystem: actor.ActorSystem = actorSystem.toClassic
+        implicit lazy val materializer: Materializer = Materializer(actorSystem)
+        Http().bindAndHandle(route, scalciteConfigurationProperties.interface, scalciteConfigurationProperties.port)
+      }
+      server <- Task
+        .fromFuture(_ => eventualBinding)
+        .tapError(
+          exception =>
+            UIO(
+              actorSystem.log.error(
+                s"Server could not start with parameters [host:port]=[${scalciteConfigurationProperties.interface},${scalciteConfigurationProperties.port}]",
+                exception
+              )
+            )
+        )
+        .forever
+        .fork
+      _ <- UIO(
+        actorSystem.log.info(
+          s"Server online at http://${scalciteConfigurationProperties.interface}:${scalciteConfigurationProperties.port}/${SwaggerApi.swagger}"
+        )
+      )
+      _ <- server.join
+    } yield ()
 
 }
