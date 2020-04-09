020341aaacc138a0aff7fd111be448317db2c586
Miguel Villafuerte <mivimur@hotmail.com>
2020-04-09 16:48:06 +0200

    refactor(scalcite-example) : integrate with zio layer

--- scalcite-example/backend/src/main/scala/io/github/mvillafuertem/scalcite/example/configuration/ScalciteServiceConfiguration.scala
+++ scalcite-example/backend/src/main/scala/io/github/mvillafuertem/scalcite/example/configuration/ScalciteServiceConfiguration.scala
@@ -3,22 +3,29 @@ package io.github.mvillafuertem.scalcite.example.configuration
 import io.github.mvillafuertem.scalcite.example.configuration.AkkaConfiguration.{ZAkkaConfiguration, ZAkkaSystemConfiguration}
 import io.github.mvillafuertem.scalcite.example.configuration.ApiConfiguration.ZApiConfiguration
 import io.github.mvillafuertem.scalcite.example.configuration.ApplicationConfiguration.ZApplicationConfiguration
-import zio.ZLayer
+import zio.{TaskLayer, ULayer}
 
 
 trait ScalciteServiceConfiguration {
 
-
-  private val value: ZLayer[Any, Nothing, ZApplicationConfiguration] =
+  private val applicationConfigurationLayer: ULayer[ZApplicationConfiguration] =
     InfrastructureConfiguration.live >>>
       ApplicationConfiguration.live
 
-  private val value1: ZLayer[Any, Throwable, ZAkkaConfiguration] =
+  private val akkaConfigurationLayer: TaskLayer[ZAkkaConfiguration] =
     InfrastructureConfiguration.live >>>
       AkkaConfiguration.live
 
-  val ZScalciteEnv: ZLayer[Any, Throwable, ZApiConfiguration with ZAkkaConfiguration with ZAkkaSystemConfiguration] =
-    ((value ++ value1 ) >>> ApiConfiguration.live) ++ (value1 >>> AkkaConfiguration.akkaSystemLayer) ++ value1
+  private val apiConfigurationLayer: TaskLayer[ZApiConfiguration] =
+    (applicationConfigurationLayer
+      ++ akkaConfigurationLayer) >>>
+      ApiConfiguration.live
 
+  private val akkaSystemLayer: TaskLayer[ZAkkaSystemConfiguration] =
+    akkaConfigurationLayer >>>
+      AkkaConfiguration.akkaSystemLayer
+
+  val ZScalciteEnv: TaskLayer[ZApiConfiguration with ZAkkaSystemConfiguration with ZAkkaConfiguration] =
+    apiConfigurationLayer ++ akkaSystemLayer ++ akkaConfigurationLayer
 
 }
