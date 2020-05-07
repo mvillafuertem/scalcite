3ac60cd2b1806606db765a2916e332382dc7291e
Miguel Villafuerte <mivimur@hotmail.com>
2020-05-07 10:46:16 +0200

    chore(Dependencies) : update artifacts version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -139,14 +139,14 @@ object Dependencies {
   lazy val zioStreams                = "dev.zio"                               %% "zio-streams"                 % Version.zio
 
   object Version {
-    val akka                      = "2.6.4"
+    val akka                      = "2.6.5"
     val akkaHttp                  = "10.1.11"
     val calcite                   = "1.22.0"
     val circe                     = "0.13.0"
     val elasticApm                = "1.15.0"
     val h2                        = "1.4.200"
-    val json4s                    = "3.6.7"
-    val jsoniter                  = "2.2.0"
+    val json4s                    = "3.6.8"
+    val jsoniter                  = "2.2.1"
     val logback                   = "1.2.3"
     val mapflablup                = "0.1.1"
     val scalaLogging              = "3.9.2"
@@ -154,7 +154,7 @@ object Dependencies {
     val scalcite                  = "0.1.1"
     val scalikejdbc               = "3.4.1"
     val sqlline                   = "1.9.0"
-    val tapir                     = "0.14.3"
+    val tapir                     = "0.14.4"
     val zio                       = "1.0.0-RC18-2"
     val zioLogging                = "0.2.8"
     val zioInteropReactiveStreams = "1.0.3.5-RC6+7-dba6c28e-SNAPSHOT"
