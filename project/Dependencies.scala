bc7f8ad5f81c8413402d57cdb3e18c38068829f6
Scala Steward <me@scala-steward.org>
2020-05-07 20:36:53 +0200

    Update zio-interop-reactivestreams to 1.0.3.5-RC7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -157,6 +157,6 @@ object Dependencies {
     val tapir                     = "0.14.4"
     val zio                       = "1.0.0-RC18-2"
     val zioLogging                = "0.2.8"
-    val zioInteropReactiveStreams = "1.0.3.5-RC6+7-dba6c28e-SNAPSHOT"
+    val zioInteropReactiveStreams = "1.0.3.5-RC7"
   }
 }
