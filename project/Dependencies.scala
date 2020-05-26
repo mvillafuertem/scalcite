d7025430b8ff44c09a0fe44ca9981a4ff1825b9d
Scala Steward <me@scala-steward.org>
2020-05-26 16:56:42 +0200

    Update tapir-akka-http-server, tapir-core, ... to 0.15.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -152,7 +152,7 @@ object Dependencies {
     val scalcite                  = "0.1.1"
     val scalikejdbc               = "3.4.2"
     val sqlline                   = "1.9.0"
-    val tapir                     = "0.15.2"
+    val tapir                     = "0.15.3"
     val zio                       = "1.0.0-RC20"
     val zioLogging                = "0.2.9"
     val zioInteropReactiveStreams = "1.0.3.5-RC9"
