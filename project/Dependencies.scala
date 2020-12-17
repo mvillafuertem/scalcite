1fbbf401bbbe9fe4ce7df829f101aafca2c61f98
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-12-17 22:52:52 +0100

    chore(Dependencies) : update akka-http, akka-http-testkit to 10.2.2 (#170)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -139,7 +139,7 @@ object Dependencies {
 
   object Version {
     val akka                      = "2.6.10"
-    val akkaHttp                  = "10.2.1"
+    val akkaHttp                  = "10.2.2"
     val calcite                   = "1.26.0"
     val circe                     = "0.13.0"
     val elasticApm                = "1.19.0"
