b662f5db0de4e157a578e89aad6bd11e93d99254
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-03-03 22:06:51 +0100

    chore(Dependencies) : update scala-library from 2.12.12 to 2.12.13 (#217)

--- project/Settings.scala
+++ project/Settings.scala
@@ -4,7 +4,7 @@ import sbt.{ Def, Tests, _ }
 object Settings {
 
   lazy val scala213               = "2.13.4"
-  lazy val scala212               = "2.12.12"
+  lazy val scala212               = "2.12.13"
   lazy val supportedScalaVersions = List(scala213, scala212)
 
   val value: Seq[Def.Setting[_]] = Seq(
