aa90097d6b6e484de9b2cd5ba5f23d4f12be6135
Scala Steward <me@scala-steward.org>
2021-04-12 06:35:23 +0200

    chore(Dependencies) : update zio, zio-streams from 1.0.5 to 1.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -154,7 +154,7 @@ object Dependencies {
     val scalikejdbc               = "3.5.0"
     val sqlline                   = "1.11.0"
     val tapir                     = "0.17.19"
-    val zio                       = "1.0.5"
+    val zio                       = "1.0.6"
     val zioLogging                = "0.5.8"
     val zioInteropReactiveStreams = "1.3.0.7-2"
   }
