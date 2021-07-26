19f6254d269a6356e60159beed5c96ac0937991a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-07-26 10:13:53 +0200

    chore(Dependencies) : update json4s-jackson from 4.0.1 to 4.0.2 (#294)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -144,7 +144,7 @@ object Dependencies {
     val circe                     = "0.14.1"
     val elasticApm                = "1.25.0"
     val h2                        = "1.4.200"
-    val json4s                    = "4.0.1"
+    val json4s                    = "4.0.2"
     val jsoniter                  = "2.9.1"
     val logback                   = "1.2.3"
     val mapflablup                = "0.1.1"
