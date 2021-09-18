8aa7ff807909e805abc7b668b2a18f6a32b823ee
Scala Steward <me@scala-steward.org>
2021-09-18 02:15:39 +0200

    chore(Dependencies) : update scalatest from 3.2.9 to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -149,7 +149,7 @@ object Dependencies {
     val logback                   = "1.2.6"
     val mapflablup                = "0.1.1"
     val scalaLogging              = "3.9.4"
-    val scalaTest                 = "3.2.9"
+    val scalaTest                 = "3.2.10"
     val scalcite                  = "0.1.1"
     val scalikejdbc               = "3.5.0"
     val sqlline                   = "1.11.0"
