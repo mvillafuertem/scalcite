f9d8a33d2bf79b2531c5ebb338213e98a4c30a0d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-12-20 19:32:43 +0100

    chore(Dependencies) : update akka-actor-testkit-typed, ... from 2.6.17 to 2.6.18 (#370)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -138,7 +138,7 @@ object Dependencies {
   lazy val zioStreams                = "dev.zio"                               %% "zio-streams"                 % Version.zio
 
   object Version {
-    val akka                      = "2.6.17"
+    val akka                      = "2.6.18"
     val akkaHttp                  = "10.2.7"
     val calcite                   = "1.28.0"
     val circe                     = "0.14.1"
