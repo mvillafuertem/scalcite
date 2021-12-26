e93b44398bc5bf173a93f0a899fff492508b13cc
Scala Steward <me@scala-steward.org>
2021-12-26 12:51:18 +0100

    chore(Dependencies) : update calcite-core from 1.28.0 to 1.29.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -140,7 +140,7 @@ object Dependencies {
   object Version {
     val akka                      = "2.6.18"
     val akkaHttp                  = "10.2.7"
-    val calcite                   = "1.28.0"
+    val calcite                   = "1.29.0"
     val circe                     = "0.14.1"
     val elasticApm                = "1.28.3"
     val h2                        = "1.4.200"
