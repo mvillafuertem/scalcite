3e6b4ca0d18833eebe3b1c806af90c68bd6a271f
Scala Steward <me@scala-steward.org>
2022-01-17 21:53:09 +0100

    chore(Dependencies) : update h2 from 1.4.200 to 2.1.210

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -143,7 +143,7 @@ object Dependencies {
     val calcite                   = "1.29.0"
     val circe                     = "0.14.1"
     val elasticApm                = "1.28.4"
-    val h2                        = "1.4.200"
+    val h2                        = "2.1.210"
     val json4s                    = "4.0.3"
     val jsoniter                  = "2.12.1"
     val logback                   = "1.2.10"
