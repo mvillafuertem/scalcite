051f8ab9978b8381bc9011064b5b917f3f7fb04f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-04-12 23:21:13 +0200

    chore(Dependencies) : update elastic-apm-agent from 1.30.0 to 1.30.1 (#437)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -142,7 +142,7 @@ object Dependencies {
     val akkaHttp                  = "10.2.9"
     val calcite                   = "1.30.0"
     val circe                     = "0.14.1"
-    val elasticApm                = "1.30.0"
+    val elasticApm                = "1.30.1"
     val h2                        = "1.4.200"
     val json4s                    = "4.0.5"
     val jsoniter                  = "2.13.13"
