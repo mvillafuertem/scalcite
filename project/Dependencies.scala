d8f45a94b0455a99690701e4c5e628802b09a5ef
Scala Steward <me@scala-steward.org>
2022-04-21 15:10:39 +0200

    chore(Dependencies) : update zio-interop-reactivestreams from 1.3.9 to 1.3.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -156,6 +156,6 @@ object Dependencies {
     val tapir                     = "0.17.20"
     val zio                       = "1.0.14"
     val zioLogging                = "0.5.14"
-    val zioInteropReactiveStreams = "1.3.9"
+    val zioInteropReactiveStreams = "1.3.10"
   }
 }
