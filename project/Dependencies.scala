b077718c4a5249565be40bccd1c5853b80555624
Scala Steward <scala_steward@virtuslab.com>
2022-07-15 18:48:30 +0000

    Revert commit(s) 703ad33

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -141,7 +141,7 @@ object Dependencies {
     val akka                      = "2.6.19"
     val akkaHttp                  = "10.2.9"
     val calcite                   = "1.30.0"
-    val circe                     = "0.14.2"
+    val circe                     = "0.14.1"
     val elasticApm                = "1.31.0"
     val h2                        = "1.4.200"
     val json4s                    = "4.0.5"
