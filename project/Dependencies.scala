20524de161d985433eda771e6a444cc76524bb25
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-15 20:51:52 +0200

    chore(Dependencies) : update jsoniter-scala-core, ... from 2.13.34 to 2.13.36 (#474)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -145,7 +145,7 @@ object Dependencies {
     val elasticApm                = "1.33.0"
     val h2                        = "1.4.200"
     val json4s                    = "4.0.5"
-    val jsoniter                  = "2.13.34"
+    val jsoniter                  = "2.13.36"
     val logback                   = "1.2.11"
     val mapflablup                = "0.1.1"
     val scalaLogging              = "3.9.5"
