9890617a17735dbd26333d1a9974a2ea5485ac16
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-02 18:03:18 +0200

    chore(Dependencies) : update calcite-core from 1.30.0 to 1.31.0 (#484)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -140,7 +140,7 @@ object Dependencies {
   object Version {
     val akka                      = "2.6.19"
     val akkaHttp                  = "10.2.9"
-    val calcite                   = "1.30.0"
+    val calcite                   = "1.31.0"
     val circe                     = "0.14.2"
     val elasticApm                = "1.33.0"
     val h2                        = "1.4.200"
