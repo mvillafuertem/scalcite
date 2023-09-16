3da40c5a7504c7d69eea028bd36ec605ee822c88
Scala Steward <scala_steward@virtuslab.com>
2023-09-16 20:28:40 +0000

    chore(Dependencies) : update scala-library from 2.13.10 to 2.13.12

--- project/Settings.scala
+++ project/Settings.scala
@@ -3,7 +3,7 @@ import sbt.{ Def, Tests, _ }
 
 object Settings {
 
-  lazy val scala213               = "2.13.10"
+  lazy val scala213               = "2.13.12"
   lazy val scala212               = "2.12.17"
   lazy val supportedScalaVersions = List(scala213, scala212)
 
